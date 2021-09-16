1c44282e9366b3b7465db70f0b268bfe67d37063
Suguru Kosaka <suguru.kosaka@exmotion.co.jp>
2021-09-16 19:04:49 +0900

    いくつか分析手法を追加

--- README.md
+++ README.md
@@ -3,53 +3,22 @@
 ``` bash
 $ python3 observe_questionaire.py 
 
-    User                            Text
-0  user1            GitHub Actions はとっても便利です。
-1  user2             GitHub Actions は使いやすくない。
-2  user3  GitHub Actions よりも Jenkins の方が便利です。
-3  user4          過去のログ同士を比較できる機能がほしいです。
-4  user5                実行時間をもっと短くして欲しい。
-
-user1 : GitHub Actions はとっても便利です。
-  → 便利
-{'p': 1, 'n': 0, 'e': 0}
-Positive 1.0
-Negative 0.0
-Neutral 0.0
-
-
-user2 : GitHub Actions は使いやすくない。
-{'p': 0, 'n': 0, 'e': 0}
-
-
-user3 : GitHub Actions よりも Jenkins の方が便利です。
-  → 便利
-{'p': 1, 'n': 0, 'e': 0}
-Positive 1.0
-Negative 0.0
-Neutral 0.0
-
-
-user4 : 過去のログ同士を比較できる機能がほしいです。
-  → 過去
-  → 機能
-{'p': 1, 'n': 1, 'e': 0}
-Positive 0.5
-Negative 0.5
-Neutral 0.0
-
-
-user5 : 実行時間をもっと短くして欲しい。
-  → 実行
-  → 時間
-{'p': 0, 'n': 0, 'e': 2}
-Positive 0.0
-Negative 0.0
+[INFO] ########## input ##########
+[INFO]                         Text  Expected
+0  GitHub Actions はとっても便利です。  positive
+1         Eureka Box は役立ってます  positive
+2   GitHub Actions は使いやすくない。  negative
+3         もっとわかりやすく説明してください。  negative
+4            あなたよりも彼の方が好きです。  negative
+5             晩御飯にカレーを食べました。   neutral
+[INFO]
+########## result ##########
+[INFO]                         Text  Expected                     mecab                    janome     asari                             oseti
+0  GitHub Actions はとっても便利です。  positive  {'p': 1, 'n': 0, 'e': 0}  {'p': 1, 'n': 0, 'e': 0}  positive  [{'positive': 1, 'negative': 0}]
+1         Eureka Box は役立ってます  positive  {'p': 0, 'n': 0, 'e': 0}  {'p': 0, 'n': 0, 'e': 0}  positive  [{'positive': 1, 'negative': 0}]
+2   GitHub Actions は使いやすくない。  negative  {'p': 0, 'n': 0, 'e': 0}  {'p': 0, 'n': 0, 'e': 0}  negative  [{'positive': 0, 'negative': 0}]
+3         もっとわかりやすく説明してください。  negative  {'p': 0, 'n': 0, 'e': 1}  {'p': 0, 'n': 0, 'e': 1}  positive  [{'positive': 0, 'negative': 0}]
+4            あなたよりも彼の方が好きです。  negative  {'p': 1, 'n': 0, 'e': 0}  {'p': 1, 'n': 0, 'e': 0}  positive  [{'positive': 1, 'negative': 0}]
+5             晩御飯にカレーを食べました。   neutral  {'p': 0, 'n': 0, 'e': 0}  {'p': 0, 'n': 0, 'e': 0}  positive  [{'positive': 0, 'negative': 0}]
 ```
 
-辞書をリッチにしないと精度が出ない？？？
-- user1 : ○
-- user2 : × (ネガティブな意見を評価できていない)
-- user3 : × (「便利」をひろってポジティブに評価されている)
-- user4 : ○ (要望なのでニュートラルな評価は妥当か？)
-- user5 : × (不満からくる要望だがニュートラルと評価されている)
--- observe_questionaire.py
+++ observe_questionaire.py
@@ -1,58 +1,174 @@
 import pandas as pd
 import csv
+import pprint
+import logging
+import warnings
+import MeCab
 from janome.tokenizer import Tokenizer
+from asari.api import Sonar
+import oseti
 
-"""
-アンケートデータ作成
-"""
-ans1 = 'DevOps CI はとっても便利です。'
-ans2 = 'DevOps CI は使いやすくない。'
-ans3 = 'DevOps CI よりも Jenkins の方が便利です。'
-ans4 = '過去のログ同士を比較できる機能がほしいです。'
-ans5 = '実行時間をもっと短くして欲しい。'
+logging.basicConfig(level=logging.INFO, format='[%(levelname)s] %(message)s')
+# logging.basicConfig(level=logging.DEBUG, format='[%(levelname)s] %(message)s')
+warnings.simplefilter('ignore')
 
-user_list = ['user1', 'user2', 'user3', 'user4','user5']
-text_list = [ans1, ans2, ans3, ans4, ans5]
+def main():
+    logging.info('########## input ##########')
+    df = get_date()
+    logging.info(df)
+    
+    logging.debug('########## mecab ##########')
+    do_mecab(df)
+    
+    logging.debug('########## janome ##########')
+    do_janome(df)
+    
+    logging.debug('########## asari ##########')
+    do_asari(df)
+    
+    logging.debug('########## oseti ##########')
+    do_oseti(df)
 
-df = pd.DataFrame(list(zip(user_list, text_list)), columns=['User', 'Text'])
-print(df)
-
-"""
-日本語評価極性辞書ダウンロード
-"""
-# curl http://www.cl.ecei.tohoku.ac.jp/resources/sent_lex/pn.csv.m3.120408.trim > pn.csv
-
-np_dic = {}
-fp = open("pn.csv", "rt", encoding="utf-8")
-reader = csv.reader(fp, delimiter='\t')
-for i, row in enumerate(reader):
-  name = row[0]
-  result = row[1]
-  np_dic[name] = result
+    logging.info('########## result ##########')
+    logging.info(df)
 
 
-"""
+def get_date():
+    """
+    アンケートデータ作成
+    """
+    ans1 = 'GitHub Actions はとっても便利です。'
+    ans2 = 'Eureka Box は役立ってます'
+    ans3 = 'GitHub Actions は使いやすくない。'
+    ans4 = 'もっとわかりやすく説明してください。'
+    ans5 = 'あなたよりも彼の方が好きです。'
+    ans6 = '晩御飯にカレーを食べました。'
 
-"""
-tok = Tokenizer()
+    text_list = [ans1, ans2, ans3, ans4, ans5, ans6]
+    expected_list = ['positive', 'positive', 'negative', 'negative', 'negative', 'neutral']
 
-for row in df.itertuples():
-    print(row.User + ' : ' + row.Text)
+    df = pd.DataFrame(list(zip(text_list, expected_list)), columns=['Text', 'Expected'])    
+    return df
 
-    res = {"p":0, "n":0, "e":0}
-    for t in tok.tokenize(row.Text):
-        #print(t)
-        bf = t.base_form
-        if bf in np_dic:
-            print('  → ' + bf)
-            r = np_dic[bf]
-            if r in res:
-                res[r] += 1
+    
+def get_dict():
+    """
+    日本語評価極性辞書ダウンロード
+    """
+    # curl http://www.cl.ecei.tohoku.ac.jp/resources/sent_lex/pn.csv.m3.120408.trim > pn.csv
+
+    np_dic = {}
+    fp = open("pn.csv", "rt", encoding="utf-8")
+    reader = csv.reader(fp, delimiter='\t')
+    for i, row in enumerate(reader):
+      name = row[0]
+      result = row[1]
+      np_dic[name] = result
+    return np_dic
+
+
+def do_mecab(df):
+    mecab = MeCab.Tagger('-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd')
+    mecab.parse('')
+    np_dic = get_dict()
+
+    judge = []
+    for row in df.itertuples():
+        logging.debug('text : ' + row.Text)
+
+        nodes = mecab.parseToNode(row.Text)
+
+        res = {"p":0, "n":0, "e":0}
+        while nodes:
+            logging.debug(nodes.surface)
+            logging.debug(nodes.feature)
+            if nodes.surface in np_dic:
+                logging.debug('match  → ' + nodes.surface)
+                r = np_dic[nodes.surface]
+                if r in res:
+                    res[r] += 1
+            nodes = nodes.next
+
+        logging.debug(res)
+        judge.append(res)
+        cnt = res["p"] + res["n"] + res["e"]
+        if cnt:
+            logging.debug("Positive" + str(res["p"] / cnt))
+            logging.debug("Negative" + str(res["n"] / cnt))
+            logging.debug("Neutral" + str(res["e"] / cnt))
+        logging.debug('\n')
+    
+    df['mecab'] = judge
+
+
+def do_janome(df):
+    tok = Tokenizer()
+    np_dic = get_dict()
+
+    judge = []
+    for row in df.itertuples():
+        logging.debug('text : ' + row.Text)
+
+        res = {"p":0, "n":0, "e":0}
+        for t in tok.tokenize(row.Text):
+            logging.debug(t)
+            bf = t.base_form
+            if bf in np_dic:
+                logging.debug('match  → ' + bf)
+                r = np_dic[bf]
+                if r in res:
+                    res[r] += 1
                 
-    print(res)
-    cnt = res["p"] + res["n"] + res["e"]
-    if cnt:    
-        print("Positive", res["p"] / cnt)
-        print("Negative", res["n"] / cnt)
-        print("Neutral" , res["e"] / cnt)
-    print('\n')
+        logging.debug(res)
+        judge.append(res)
+        cnt = res["p"] + res["n"] + res["e"]
+        if cnt:    
+            logging.debug("Positive" + str(res["p"] / cnt))
+            logging.debug("Negative" + str(res["n"] / cnt))
+            logging.debug("Neutral" + str(res["e"] / cnt))
+        logging.debug('\n')
+    df['janome'] = judge
+
+
+def do_asari(df):
+    """
+    https://jupyterbook.hnishi.com/language-models/easy_try_sentiment_analysis.html#asari
+    https://teratail.com/questions/316611
+    """
+    sonar = Sonar()
+    
+    judge = []
+    for row in df.itertuples():
+        logging.debug('text : ' + row.Text)
+        res = sonar.ping(row.Text)
+        judge.append(res['top_class'])
+        #pprint.pprint(res)
+        logging.debug(res['top_class'])
+        logging.debug("Positive" + str(res['classes'][1]['confidence']))
+        logging.debug("Negative" + str(res['classes'][0]['confidence']))
+        logging.debug('\n')
+    df['asari'] = judge
+
+
+def do_oseti(df):
+    """
+    janome で指定したのと同じ辞書を使ってるので結果も同じ。
+    !pip install mecab-python3==0.996.5
+    https://qiita.com/m__k/items/50b018c60f952c28869e
+    """
+    analyzer = oseti.Analyzer()
+
+    judge = []
+    for row in df.itertuples():
+        logging.debug('text : ' + row.Text)
+        res_count = analyzer.count_polarity(row.Text)
+        judge.append(res_count)
+        logging.debug(res_count)
+        res_detail = analyzer.analyze_detail(row.Text)
+        logging.debug(res_detail)
+        logging.debug('\n')
+    df['oseti'] = judge
+    
+
+if __name__ == '__main__':
+    main()
